218a7da26aebfbcb2b19f59469d37fc120f02072
OMGitsNIK <raonik1003@gmail.com>
2025-06-05 20:20:02 +0530

    added megaeth testnet

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { anvil, zksync, mainnet, sepolia } from "wagmi/chains";
+import { anvil, zksync, mainnet, sepolia, megaethTestnet } from "wagmi/chains";
 
 export default getDefaultConfig({
   appName: "TSender",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [anvil, zksync, mainnet, sepolia],
+  chains: [anvil, zksync, mainnet, sepolia, megaethTestnet],
   ssr: false,
 });
